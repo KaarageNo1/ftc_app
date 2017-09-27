52b4c0a7aebb6958b2631c0c6f09bf877b5881f2
GAMEPC\Dani <damageq12@gmail.com>
2017-09-27 17:33:14 +0300

    Program de baza (Main Loop)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by Dani on 9/27/2017.
+ */
+
+@TeleOp(name = "TeleOp Test", group = "Tests")
+
+
+public class TeleOpTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+
+        }
+    }
+}
