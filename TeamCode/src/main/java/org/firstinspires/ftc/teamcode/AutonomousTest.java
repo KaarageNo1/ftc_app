76f1a88480be3ca44a7d6378d8ab541256949c0d
T400\Dani <damageq12@gmail.com>
2017-09-28 10:13:44 +0300

    Added turnToLeftDegrees function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 
 /**
  * Created by Dani on 9/27/2017.
@@ -18,6 +19,8 @@ public class AutonomousTest extends LinearOpMode {
     DcMotor motorFrontLeft = null;
     DcMotor motorRearRight = null;
     DcMotor motorRearLeft = null;
+
+    GyroSensor gyro = null;
 
 
     @Override
@@ -38,9 +41,13 @@ public class AutonomousTest extends LinearOpMode {
 
         waitForStart();
 
+        gyro.calibrate();
+
         moveForwardTime(MOVE_SPEED, 5000);
-        turnLeftTime(MOVE_SPEED, 3000);
+       // turnLeftTime(MOVE_SPEED, 3000);
         moveForwardTime(MOVE_SPEED, 1000);
+        //turnLeftGyro(MOVE_SPEED);
+        turnToLeftDegrees(MOVE_SPEED, 270);
         stopMovement();
     }
 
@@ -62,14 +69,31 @@ public class AutonomousTest extends LinearOpMode {
     }
 
 
-    public void turnLeftTime(double power,long time) throws InterruptedException{
+    public void turnLeft(double power){
         motorFrontRight.setPower(power);
         motorFrontLeft.setPower(-power);
         motorRearLeft.setPower(power);
         motorRearRight.setPower(-power);
+    }
+
+    /*public void turnLeftTime(double power,long time) throws InterruptedException{
+        turnLeft(power);
 
         Thread.sleep(time);
+    }*/
+
+    /*public void turnLeftGyro(double power){
+        while(gyro.getHeading() > 270 || gyro.getHeading() < 260) {
+            turnLeft(power);
+        }
+    }*/
+
+    public void turnToLeftDegrees(double power, double degree){
+        while(gyro.getHeading() != degree) {
+            turnLeft(power);
+        }
     }
+
 
     public void stopMovement(){
         moveForward(0);
